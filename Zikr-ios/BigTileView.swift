a900ad8899d0353fcd6621f11d181ec010fda419
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-05-02 20:31:34 +0900

    feat: add big tile view

---
+++ Zikr-ios/BigTileView.swift
@@ -0,0 +1,37 @@
+//
+//  BigTileView.swift
+//  Zikr-ios
+//
+//  Created by Саид-Насир Исмаилов on 2023/05/02.
+//
+
+import SwiftUI
+
+struct BigTileView: View {
+    let title: String
+    let imageName: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(systemName: imageName)
+                .font(.system(size: 30))
+                .foregroundColor(.black)
+
+            
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.black)
+                .fontWeight(.bold)
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(20)
+        .shadow(radius: 5)
+    }
+}
+
+struct BigTileView_Previews: PreviewProvider {
+    static var previews: some View {
+        BigTileView(title: "Morning", imageName: "sun.max.circle.fill")
+    }
+}
