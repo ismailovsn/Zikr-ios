1f7c44a503c732c9f848c6345b46c5b6491abed3
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-05-02 20:25:41 +0900

    Initial Commit

---
+++ Zikr-ios/Zikr_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zikr_iosApp.swift
+//  Zikr-ios
+//
+//  Created by Саид-Насир Исмаилов on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct Zikr_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
